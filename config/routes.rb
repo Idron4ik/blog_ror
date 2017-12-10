4c50bf9dacdf0bcd2e9ce6a8c0e5618294425b6a
Unknown <vi.chybirka@gmail.com>
2017-12-10 21:15:47 +0200

    Preload
    Add a bit polite

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get "/about", to: "pages#about"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
